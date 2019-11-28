16a0bd3def8d3f4f9d87d45b0755b0502883cf50
Alex Trujillo <a01328426@itesm.mx>
2019-11-27 21:05:28 -0600

    commit

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
-exports.DATABASE_URL = process.env.DATABASE_URL || "mongodb://localhost/usersDB";
+exports.DATABASE_URL = process.env.DATABASE_URL || "mongodb://localhost/UserDB";
 exports.PORT = process.env.PORT || 8080;
